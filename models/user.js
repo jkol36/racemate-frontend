07e819089a2c07ab2e64aacc86df1880df86da16
Jon Kolman <jonathankolman@gmail.com>
2017-02-08 13:17:00 +0000

    rough user model schema

---
+++ models/user.js
@@ -0,0 +1,13 @@
+const initialUser = {
+  displayName: '',
+  notificationOptions: [],
+  email: '',
+  autoBet: false,
+  presets: {},
+  subscription: {
+    permissionLevel: 0
+  }
+
+}
+
+export default initialUser
